9d5d0d0b2ac28862a42d5b99af97e2caca97552c
Debanjan <debanjanacharya21@gmail.com>
2021-09-05 18:26:44 +0530

    05-09

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react'
 import AboutUs from './pages/AboutUs'
 //import styles
 import GlobalStyle from './components/GlobalStyle';
+import Nav from './components/Nav';
 
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutUs/>
     </div>
   );
